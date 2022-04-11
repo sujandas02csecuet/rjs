b3ade53dcb73ab3a13c2430bbab5452e55d4c01b
Sujan Das <sujandas02csecuet@gmail.com>
2022-04-11 22:25:26 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import './App.css';
+
 
 function App() {
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then((Response)=>Response.json)
-.then((json)=>console.log(json))
+
+
+
+
 
   return (
     <div>
-      <h1>Let us  Make the world a better place to live</h1>
+    
      
     </div>
   );
