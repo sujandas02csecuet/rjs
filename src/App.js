fa0906cc50618d71e1e277b90a77245085a33424
Sujan Das <sujandas02csecuet@gmail.com>
2022-04-11 21:21:29 +0600

    api integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function App() {
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((Response)=>Response.json)
+.then((json)=>console.log(json))
+
   return (
     <div>
       <h1>Let us  Make the world a better place to live</h1>
